b2c2232ccd6fb8c28696e8769ee9e1ed65259ed7
myles-coleman <mylescoleman05@gmail.com>
2025-10-23 19:59:50 -0700

    feat: add argo sync waves to control deployment order

--- generate-apps.sh
+++ generate-apps.sh
@@ -13,11 +13,29 @@ resources=$(grep "^- " "$CLUSTER_KUSTOMIZATION" | sed 's/^- //')
 declare -A namespace_map
 namespace_map["metallb"]="metallb-system"
 
+# Sync wave mapping for deployment order (lower numbers deploy first)
+declare -A sync_wave_map
+# Wave 0: Core infrastructure
+sync_wave_map["external-secrets"]="0"
+sync_wave_map["cert-manager"]="1"
+# Wave 2: Network infrastructure
+sync_wave_map["traefik"]="2"
+sync_wave_map["metallb"]="2"
+# Wave 3: Storage
+sync_wave_map["longhorn"]="3"
+# Wave 4: Auth
+sync_wave_map["dex"]="4"
+# Wave 10: Applications (default)
+# All other apps will use wave 10
+
 for resource in $resources; do
   echo "Generating Application for $resource..."
   
   # Use mapped namespace if exists, otherwise use resource name
   target_namespace="${namespace_map[$resource]:-$resource}"
+  
+  # Use mapped sync wave if exists, otherwise use default (10)
+  sync_wave="${sync_wave_map[$resource]:-10}"
   
   cat > "$BOOTSTRAP_DIR/$resource-app.yaml" << EOF
 apiVersion: argoproj.io/v1alpha1
@@ -25,6 +43,8 @@ kind: Application
 metadata:
   name: $resource
   namespace: argocd
+  annotations:
+    argocd.argoproj.io/sync-wave: "$sync_wave"
 spec:
   destination:
     server: https://kubernetes.default.svc
