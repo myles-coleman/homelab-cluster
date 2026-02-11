eb8a4f76a4d6bc52a52dba0bcedd82aa56c0fde5
Myles Coleman <44505855+myles-coleman@users.noreply.github.com>
2026-02-10 17:11:17 -0800

    chore: set allowed emails as env var (#3)
    * chore: set allowed emails as env var
    
    * chore: remove authentik
    
    * chore: make pr commenter quiet

--- modules/authentik/flows.tf
+++
@@ -1,7 +0,0 @@
-data "authentik_flow" "default_authorization" {
-  slug = "default-provider-authorization-implicit-consent"
-}
-
-data "authentik_flow" "default_invalidation" {
-  slug = "default-provider-invalidation-flow"
-}
--- modules/authentik/outpost.tf
+++
@@ -1,8 +0,0 @@
-resource "authentik_outpost" "traefik" {
-  name = "traefik-forward-auth"
-  type = "proxy"
-
-  protocol_providers = [
-    for provider in authentik_provider_proxy.apps : provider.id
-  ]
-}
