82d42c5b3a6ede964bb1580659bcc82faed246fa
Myles Coleman <mylescoleman05@gmail.com>
2026-05-16 12:53:55 -0700

    chore: add flake and .envrc

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "homelab-cluster development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go-task
+              kustomize
+              yq-go
+              helmfile
+              kubectl
+            ];
+          };
+        });
+    };
+}
